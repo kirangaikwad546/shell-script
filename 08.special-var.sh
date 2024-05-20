2e3675007db818c005c1d201ebb6500c1d19494b
kirangaikwad546 <gkiran546@gmail.com>
2024-05-20 12:24:55 +0530

    hello script

--- 08.special-var.sh
+++ 08.special-var.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "All Variables: $@"
+echo "Number of variables passed: $#"
+echo "script Name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "Process ID of the current shell script: $$"
+echo "process ID of lasr background command: $!"
